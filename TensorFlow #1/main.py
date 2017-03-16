d99ddae3e0d8e17596d8e931ab7c7fc00bf735fa
Slyces <simon.lassourreuille@gmail.com>
2017-03-16 01:06:25 +0100

    Je commence un petit Hello World tensorflow vraiment sympa ! -> Sur Jupyter Python, en plus, c'est vraiment pas mal, je vous le conseille, ça s'installe en 1 ligne. MDR.
    Signed-off-by: Slyces <simon.lassourreuille@gmail.com>

--- TensorFlow #1/main.py
+++ TensorFlow #1/main.py
@@ -8,4 +8,12 @@ tensor en sortie.
 # Exemple de noeud constant
 node1 = tf.constant(3.0, tf.float32)
 node2 = tf.constant(4.0) # also tf.float32 implicitly
-print(node1, node2)
+print("Print un Node, ça ressemble à ça :")
+print(" "*4 + "-", node1)
+print(" "*4 + "-", node2)
+print("--> ça print les objets")
+
+print("\nUne session est nécessaire pour encapsuler et éxécuter un graphe")
+sess = tf.Session()
+print("sess.run([node1, node2]):", sess.run([node1, node2]))
+
