4d3b27cc6745cbcc06cb406fbaad855c364758cf
Slyces <simon.lassourreuille@gmail.com>
2017-03-28 13:16:55 +0200

    Test de Bokeh !! ça marche en local <3
    Signed-off-by: Slyces <simon.lassourreuille@gmail.com>

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,13 +1,17 @@
 # from django.shortcuts import get_object_or_404, render
 # from django.http import HttpResponseRedirect
 # from django.urls import reverse
+
+# Django
 from django.views import generic
 from hello_world.models import Messages
-
 from django.shortcuts import render
+
+# Bokeh
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import components
+from hello_world.Scripts import Visualize
 
 
 def simple_chart(request):
@@ -16,12 +20,26 @@ def simple_chart(request):
 
     script, div = components(plot, CDN)
 
+    print(script, div)
+
     return render(request, "hello_world/simple_chart.html",
-                  {"the_script": script, "the_div": div})
+                  {"script": script, "div": div})
 
 
-class IndexView(generic.ListView):
+class IndexView(generic.base.TemplateView):
     template_name = 'hello_world/index.html'
+    context_object_name = 'context'
+    model = Messages
 
-    def get_queryset(self, *args, **kwargs):
-        pass
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        indexes = 'Nasdaq DowJones S&P500 Rates'.split()
+        plots = Visualize.plot_feedforward(path="hello_world/Scripts/")
+
+        for index in indexes:
+            script, div = components(plots[index], CDN)
+            context[index + '_script'] = script
+            context[index + '_div'] = div
+
+        return context
