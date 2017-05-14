642799fa5df5d6121bd390fdd75241d0e751df9b
Slyces <simon.lassourreuille@gmail.com>
2017-05-14 15:08:00 +0200

    - Une bannière expérimentale + menu sticky fini + mise en page de finance
    Signed-off-by: Slyces <simon.lassourreuille@gmail.com>

--- hello_world/static/hello_world/myscript.js
+++ hello_world/static/hello_world/myscript.js
@@ -1,8 +1,16 @@
-// $(document)
-//     .ready(function () {
-//         // pass
-//     })
-// ;
+$(document)
+    .ready(function () {
+        // Dropdown
+        $('.ui.dropdown')
+            .dropdown();
 
-$('.ui.dropdown')
-  .dropdown();
+        // Sticky menu
+        $('.main.menu')
+            .visibility({
+                type: 'fixed'
+            });
+
+        // Scroll up
+        // $('#lol').click($('#menu').animatescroll());
+    })
+;
