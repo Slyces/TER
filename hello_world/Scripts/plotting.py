f9715e5939dcc92ff1a3c6fbcf18f53dd83b76bf
Slyces <simon.lassourreuille@gmail.com>
2017-05-16 22:26:37 +0200

    - Modification du plot du feed forward pour repasser les données en normalisé
    Signed-off-by: Slyces <simon.lassourreuille@gmail.com>

--- hello_world/Scripts/plotting.py
+++ hello_world/Scripts/plotting.py
@@ -7,6 +7,7 @@ from math import pi
 from django_world.settings import DATABASE_PATH
 
 def plot_feedforward(label):
+    indexes = 'Nasdaq DowJones SnP500 Rates'.split()
     with sqlite3.connect(DATABASE_PATH) as con:
         cursor = con.cursor()
 
@@ -22,13 +23,32 @@ def plot_feedforward(label):
             label
         )
         cursor.execute(select_predictions)
-        predictions = np.array(cursor.fetchall())
+        predicted = np.array(cursor.fetchall())
+
+        extr = dict([(index, {}) for index in indexes])
+        for index in indexes:
+            for m in 'min max'.split():
+                cursor.execute("""SELECT {} FROM extremum_indexes
+                                  WHERE ind = ?""".format(m), (index,))
+                extr[index][m] = cursor.fetchone()[0]
+
+    # De-normalize datas
+    # Data normalization formula : y = (x - Min(X))/(Max(X) - Min(X))
+    # To de-normalize, we use : (Max(X) - Min(X)) * y + Min(X)
+    def f(x, extrems):
+        return (extrems['max'] - extrems['min']) * x + extrems['min']
+
+
+    for i, index in enumerate(indexes):
+        historic[:, i] = f(historic[:, i], extr[index])
+        predicted[:, i] = f(predicted[:, i], extr[index])
+
+    error = np.square(historic - predicted)
 
     width = 800
     height = 350
 
     axes = [None for i in range(4)]
-    indexes = 'Nasdaq DowJones SnP500 Rates'.split()
     for i in range(4):
         TOOLS = 'pan,wheel_zoom,box_zoom,resize,reset'
 
@@ -42,7 +62,7 @@ def plot_feedforward(label):
             'line_join': 'round'
         }
 
-        axes[i].line(dates, predictions[:, i], legend="Predictions", line_color='navy', **options)
+        axes[i].line(dates, predicted[:, i], legend="Predictions", line_color='navy', **options)
         axes[i].line(dates, historic[:, i], legend="Historic Data", color='red', **options)
 
         axes[i].xaxis.formatter = DatetimeTickFormatter(
@@ -52,7 +72,10 @@ def plot_feedforward(label):
         )
         axes[i].xaxis.major_label_orientation = pi / 4
         axes[i].xaxis.bounds = (dates[0], dates[-1])
-        axes[i].yaxis.bounds = (0, 1)
+        print((extr[indexes[i]]['min'],
+                                extr[indexes[i]]['max']))
+        axes[i].yaxis.bounds = (extr[indexes[i]]['min'],
+                                extr[indexes[i]]['max'])
 
         axes[i].legend.border_line_width = 1
         axes[i].legend.border_line_color = "black"
